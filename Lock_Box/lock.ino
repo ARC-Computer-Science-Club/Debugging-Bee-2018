7b2a5e9116358d67b4349beab1085ea787ffd7d6
Kevin <kmkirby90@gmail.com>
2018-11-07 00:34:14 +0000

    changes to lock.ino

--- Lock_Box/lock.ino
+++ Lock_Box/lock.ino
@@ -1,6 +1,6 @@
-int black = 2;
-int white = 3;
-int green = 4;
+int green = 2;
+int black = 3;
+int white = 4;
 int lock = 5;
 bool state1 = false;
 bool state2 = false;
@@ -11,29 +11,70 @@ void setup() {
   pinMode(white, INPUT);
   pinMode(green, INPUT);
   pinMode(lock, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(black) == HIGH)
+  int arr[3] = {black,white,green};
+  if(combo(arr, 3))
   {
-    state1 = !(state1);
-  }
-  if(digitalRead(white) == HIGH)
-  {
-    state2 = !(state2);
-  }
-  if(digitalRead(green) == HIGH)
-  {
-    state3 = !(state3);
-  }
-
-  if(state1 && state2 && state3)
-  {
-    digitalWrite(lock, HIGH);  
+    digitalWrite(lock, LOW);
   }
   else
   {
-    digitalWrite(lock, LOW);
+    digitalWrite(lock, HIGH);
+  }
+}
+
+bool combo(int *myCombo, int comboLength)
+{
+  if(comboLength == 0)
+  {
+    return true;
+  }
+  else
+  {
+    do
+    {
+      state1 = digitalRead(black);
+      state2 = digitalRead(white);
+      state3 = digitalRead(green);
+    }while(!state1 && !state2 && !state3);
+    delay(400);
+    Serial.println(*myCombo);
+    int count = 0;
+    bool valid = false;
+    if(state1)
+    {
+      count++;
+      if(*myCombo == black)
+      {
+        valid = true;
+      }
+    }
+    if(state2)
+    {
+      count++;
+      if(*myCombo == white)
+      {
+        valid = true;
+      }
+    }
+    if(state3)
+    {
+      count++;
+      if(*myCombo == green)
+      {
+        valid = true;
+      }
+    }
+    if(count == 1 && valid)
+    {
+      return combo(myCombo+1, comboLength-1);
+    }
+    else
+    {
+      return false;
+    }
   }
 }
