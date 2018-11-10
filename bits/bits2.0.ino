238915227b5e83d25171f299f8dc275aa4b7fcf4
Kevin <kmkirby90@gmail.com>
2018-11-10 18:56:22 +0000

    variable name changes. added second parseint() call to deal with bug where 0 would be entered after every input. Check this on event machines.

---
+++ bits/bits2.0.ino
@@ -0,0 +1,42 @@
+const int NUMBER_OF_BITS = 4;
+const int bits[4] = {5,4,3,2};
+int input;
+int mask;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for(int i = 0; i < NUMBER_OF_BITS; i++)
+  {
+    pinMode(bits[i], OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) 
+  {
+    input = Serial.parseInt();
+    
+    //clear buffer
+    Serial.parseInt();
+    
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(input);
+    
+    mask = 1;
+    for(int i = 0; i < NUMBER_OF_BITS; i++)
+    {
+      if(input & mask)
+      {
+        digitalWrite(bits[i], HIGH);
+      }
+      else
+      {
+        digitalWrite(bits[i], LOW);
+      }
+      mask <<= 1;
+    }
+  }
+}
